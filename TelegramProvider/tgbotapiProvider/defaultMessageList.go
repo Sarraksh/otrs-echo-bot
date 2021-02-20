c61190f76ea424a99c471fa6325ebfa86e25e15a
Sarraksh <55298589+Sarraksh@users.noreply.github.com>
2021-02-20 06:55:11 +0300

    Add default messages

---
+++ TelegramProvider/tgbotapiProvider/defaultMessageList.go
@@ -0,0 +1,35 @@
+package tgbotapiProvider
+
+const (
+	invalidCommandResponse string = `Неверная команда. Для отображения списка команд используйте /help .`
+	helpCommandResponse    string = `Список доступных команд:
+
+Управление подписками на события
+/subscribeTeam1
+/subscribeTeam2
+/subscribeTeam3
+/unsubscribeTeam1
+/unsubscribeTeam2
+/unsubscribeTeam3
+
+Указание своих имени и фамилии
+/firstName Имя
+/lastName Фамилия
+
+Вывод данного сообщения
+/help
+`
+	startCommandResponse string = `Для начала работы с ботом пожалуйста оформите подписку на события одну из команд:
+/subscribeTeam1
+/subscribeTeam2
+/subscribeTeam3
+
+Оформленную подписку можно отменить в любой момент аналогичными командами:
+/unsubscribeTeam1
+/unsubscribeTeam2
+/unsubscribeTeam3
+
+Для автоматического получения сообщений по всем событиям для дежурных пожалуйста укажите свои имя и фамилию с помощью следующих команд:
+/firstName Имя
+/lastName Фамилия`
+)
