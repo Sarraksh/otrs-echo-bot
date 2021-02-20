7f45668bfae736540573b9e157ae540968ecdb25
Sarraksh <55298589+Sarraksh@users.noreply.github.com>
2021-02-20 06:02:11 +0300

    Main add sigterm and correct logger initialisation

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"github.com/Sarraksh/otrs-echo-bot/common/logger"
+	"github.com/Sarraksh/otrs-echo-bot/common/logger/zapLogger"
 	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 )
 
 func main() {
@@ -13,8 +17,10 @@ func main() {
 		log.Println("unable get program directory")
 	}
 	var logModule logger.Logger
-	logModule = logModule.NewDefault(filepath.Join(programDirectory, "log", "otrs-echo-bot"))
+	logModule = zapLogger.NewDefault(filepath.Join(programDirectory, "log", "otrs-echo-bot"))
 
+	logModule = logModule.SetModuleName("MainRoutine")
+	logModule.Info("Stop program")
 }
 
 // Get program directory from arguments or arguments and working directory.
@@ -29,4 +35,19 @@ func getProgramDir() (string, error) {
 	}
 	relativeDir := filepath.Dir(argumentsFilePath)
 	return filepath.Join(workingDirectory, relativeDir), nil
+}
+
+// Handle system terminal signals.
+func Sigterm(ctx context.Context, cancel context.CancelFunc) error {
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	select {
+	case sig := <-signalChannel:
+		log.Printf("Received signal: %v", sig)
+		cancel()
+	case <-ctx.Done():
+		log.Printf("Closing signal goroutine")
+		return ctx.Err()
+	}
+	return nil
 }
