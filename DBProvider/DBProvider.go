26483d6d61fd27b92e4b4fa75c63709541e05b1d
Sarraksh <55298589+Sarraksh@users.noreply.github.com>
2021-03-01 06:54:14 +0300

    Fill DBProvider method list

--- DBProvider/DBProvider.go
+++ DBProvider/DBProvider.go
@@ -1,4 +1,18 @@
 package DBProvider
 
+import "github.com/Sarraksh/otrs-echo-bot/common/logger"
+
 type DBProvider interface {
+	Initialise(logger logger.Logger, directory string) error
+
+	OTRSEventCreateNew(Channel, Type, TicketID string) error
+	OTRSEventGetActive() (int64, string, error)
+	OTRSEventGetEarliestActivationTimestamp() (int64, error)
+	OTRSEventSuspend(id int64) error
+	OTRSEventEnded(id int64) error
+
+	BotUserAdd(tgID int64) error
+	BotUserUpdateFirstName(tgID int64, firstName string) error
+	BotUserUpdateLastName(tgID int64, lastName string) error
+	BotUserGetByTelegramID(tgID int64) (int64, error)
 }
